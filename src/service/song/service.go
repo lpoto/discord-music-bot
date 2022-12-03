6f0ff6e529c635ed761aadb6cf3549b0ac40df27
lpoto <luka.potochnik@gmail.com>
2022-12-03 21:00:45 +0100

    Update song shuffling fun

--- src/service/song/service.go
+++ src/service/song/service.go
@@ -16,31 +16,31 @@ func NewSongService() *SongService {
 // ShuffleSongs shuffles the positions of the provided songs without
 // adding any new positions or chaning any of the other fields. The position
 // of the song with the minimum position is not changed.
+// NOTE: this is performed in place.
+// NOTE: this assumes the provided songs are ordered ascendingly by their positions.
 func (service *SongService) ShuffleSongs(songs []*model.Song) []*model.Song {
 	// NOTE: no point  in shuffling less than 3 songs, as they
 	// won't be shuffled
-	if len(songs) < 3 {
-		return songs
-	}
-	// NOTE: find the id of the song with minum position
-	minPos, minID := -1, -1
-	for _, song := range songs {
-		if minID == -1 || song.Position < minPos {
+	shuffled := make(map[uint]int)
 
-			minPos, minID = song.Position, int(song.ID)
-		}
-	}
-	for _, song := range songs {
-		if song.ID == uint(minID) {
+	for idx, song := range songs {
+		if _, ok := shuffled[song.ID]; ok || idx == 0 {
 			continue
 		}
-		song2 := songs[rand.Intn(len(songs))]
-		for song2.ID == uint(minID) {
-			song2 = songs[rand.Intn(len(songs))]
+
+		var song2 *model.Song
+		for i := 0; i < 100; i++ {
+			song2 = songs[rand.Intn(len(songs)-1)+1]
+			if v, ok := shuffled[song2.ID]; (!ok || v != song.Position) &&
+				song2.ID != song.ID {
+				break
+			}
 		}
-		temp := song.Position
-		song.Position = song2.Position
-		song2.Position = temp
+		shuffled[song.ID] = song.Position
+		if _, ok := shuffled[song2.ID]; !ok {
+			shuffled[song2.ID] = song2.Position
+		}
+		song.Position, song2.Position = song2.Position, song.Position
 	}
 	return songs
 }
